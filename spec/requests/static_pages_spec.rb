9f728b54dea810fc4cf2f2e31446bc798acf4687
hugh <metalzzang@gmail.com>
2013-12-14 13:34:31 +0900

    add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -36,5 +39,17 @@ describe "StaticPages" do
 			visit '/static_pages/about'
 			expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
 		end	
+	end	
+
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
+		end
 	end	
 end
