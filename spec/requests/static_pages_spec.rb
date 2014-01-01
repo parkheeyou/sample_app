05c64ff8c6c403aa41e0bafdd23c80bfa3da3775
hugh <metalzzang@gmail.com>
2014-01-01 23:10:52 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,60 +1,90 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+#	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+#
+#  describe "Home page" do
+#    it "should have the content 'Sample App'" do
+#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#     visit root_path
+#		 expect(page).to have_content('Sample App')
+#    end
+#
+#		it "should have the base title" do
+#			visit root_path
+#			expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+#		end													 	 
+#
+#		it "should not have a custom page title" do
+#			visit root_path
+#			expect(page).not_to have_title(' | Home')
+#		end	
+#  end
+#
+#	describe "Help page" do
+#		it "should have the content 'Help'" do
+#			visit help_path
+#			expect(page).to have_content('Help')
+#		end
+#
+#		it "should have the title 'Help'" do
+#			visit help_path
+#			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+#		end	
+#	end	
+#
+#	describe "About page" do
+#		it "should have the content 'About Us'" do
+#			visit about_path
+#			expect(page).to have_content('About Us')
+#		end
+#
+#		it "should have the title 'About Us'" do
+#			visit about_path
+#			expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+#		end	
+#	end	
+##
+##	describe "Contact page" do
+##		it "should have the content 'Contact'" do
+##			visit contact_path
+##			expect(page).to have_content('Contact')
+##		end
+##
+##		it "should have the title 'Contact'" do
+##			visit contact_path
+##			expect(page).to have_title("#{base_title} | Contact")
+##		end
+##	end	
+
+	subject { page }
 	
-	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	describe "Home page" do
+		before { visit root_path }
 
-  describe "Home page" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-     visit '/static_pages/home'
-		 expect(page).to have_content('Sample App')
-    end
-
-		it "should have the base title" do
-			visit '/static_pages/home'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App')
-		end													 	 
-
-		it "should not have a custom page title" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title(' | Home')
-		end	
-  end
+		it { should have_content('Sample App') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title(' |Home') }
+	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+		before { visit help_path }
 
-		it "should have the title 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
-		end	
-	end	
+		it { should have_content('Help') }
+		it { should have_title(full_title('Help')) }
+	end
 
 	describe "About page" do
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
+		before { visit about_path }
 
-		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
-		end	
-	end	
+		it { should have_content('About') }
+		it { should have_title(full_title('About')) }
+	end
 
 	describe "Contact page" do
-		it "should have the content 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_content('Contact')
-		end
+		before { visit contact_path }
 
-		it "should have the title 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_title("#{base_title} | Contact")
-		end
+		it { should have_content('Contact') }
+		it { should have_title(full_title('Contact')) }
 	end	
 end
